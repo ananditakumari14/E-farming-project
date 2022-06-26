704245906ecc72ebbad7f8a4093dee6165dcc9cf
Anandita kumari <75248882+ananditakumari14@users.noreply.github.com>
2022-06-26 19:02:41 +0530

    Add files via upload

---
+++ viewmarketplace.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-viewmarketplace',
+  templateUrl: './viewmarketplace.component.html',
+  styleUrls: ['./viewmarketplace.component.css']
+})
+export class ViewmarketplaceComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
